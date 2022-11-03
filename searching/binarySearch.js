74a12f7e7ce05ab6e5819970426f5a7b0ecb76e6
Agent Ebimene <ebimeneagent@gmail.com>
2022-11-03 17:30:34 +0100

    Add up binary search implementation using recursion

--- searching/binarySearch.js
+++ searching/binarySearch.js
@@ -16,4 +16,22 @@ function binarySearch(arr, element) {
   return -1;
 }
 
-console.log(binarySearch([3, 5, 6, 7, 89, 1, 0], 89));
+// console.log(binarySearch([3, 5, 6, 7, 89, 1, 0], 89));
+
+function recursiveBinarySearch(key, arr) {
+  const sortedArr = arr.sort((a, b) => a - b);
+
+  let mid = Math.floor(sortedArr.length / 2);
+  if (sortedArr.length === 1 && sortedArr[0] !== key) {
+    return -1;
+  }
+  if (sortedArr[mid] === key) {
+    return mid;
+  } else if (key < sortedArr[mid]) {
+    return recursiveBinarySearch(key, sortedArr.slice(0, mid));
+  } else if (key > sortedArr[mid]) {
+    return recursiveBinarySearch(key, sortedArr.slice(mid));
+  }
+}
+
+console.log(recursiveBinarySearch(3, [3, 5, 6, 7, 89, 1, 0]));
