bd7bc95afe0a8b6c6f623d13e7ed803a54191328
Agent Ebimene <ebimeneagent@gmail.com>
2022-11-19 21:52:03 +0100

    Add up optimized burble sort with a boolean variable(hasSwapped) to keep to avoid unnecessary iterations

--- sorting/burbleSort.js
+++ sorting/burbleSort.js
@@ -18,4 +18,26 @@ const burbleSort = (arr) => {
   return arr;
 };
 
-console.log(burbleSort([5, 2, 4, 6, 0, 1]));
+const optimizedBubbleSort = (arr) => {
+  let hasSwapped = false;
+  let outerLoopIterationCount = 0;
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = 0; j < arr.length - 1; j++) {
+      if (arr[j] > arr[j + 1]) {
+        swap(arr, j, j + 1);
+        hasSwapped = true;
+      }
+    }
+    if (!hasSwapped) {
+      return outerLoopIterationCount;
+    } else {
+      hasSwapped = false;
+    }
+    outerLoopIterationCount++;
+  }
+  return outerLoopIterationCount;
+};
+
+console.log(optimizedBubbleSort([2, 5, 7, 8, 9, 0]));
+console.log(optimizedBubbleSort([1, 0, 7, 8, 9, 10]));
+console.log(optimizedBubbleSort([0, 1, 7, 8, 9, 10]));
