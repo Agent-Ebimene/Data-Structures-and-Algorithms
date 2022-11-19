6ab0b8d433de521df52b074565670891b0f902d4
Agent Ebimene <ebimeneagent@gmail.com>
2022-11-19 18:40:20 +0100

    Add up burble common solution of burble sort to repo

---
+++ sorting/burbleSort.js
@@ -0,0 +1,17 @@
+//Trivial Solution
+
+const burbleSort = (arr) => {
+  const len = arr.length;
+  for (let i = 0; i < len; i++) {
+    for (let j = 0; j < len; j++) {
+      if (arr[j] > arr[j + 1]) {
+        let temp = arr[j];
+        arr[j] = arr[j + 1];
+        arr[j + 1] = temp;
+      }
+    }
+  }
+  return arr;
+};
+
+console.log(burbleSort([5, 2, 4, 6, 0, 1]));
