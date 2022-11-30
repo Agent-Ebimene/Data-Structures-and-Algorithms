bf9b843e050f9a115d3e77a8cb8c1d5f76e01d8d
Agent Ebimene <ebimeneagent@gmail.com>
2022-11-30 12:03:51 +0100

    Selection sort in a sligthly different way

--- selectionSort/selectionSort.js
+++ selectionSort/selectionSort.js
@@ -4,7 +4,7 @@
 // create a variable to store the assumed index of the lowest number (the first element);
 // create a second loop to compare elements with the variable created above starting from the next element;
 // if the element is smaller than the variable, change index to the index of that element
-// swap the position of the element found with the variable(index of the assumed smallest element);
+// swap the position of the element found with the variable's(index of the assumed smallest element);
 // Repeat;
 
 function selectionSort(arr) {
@@ -20,4 +20,22 @@ function selectionSort(arr) {
   return arr;
 }
 
-console.log(selectionSort([9, 2, 4, 6, 0]));
+function anotherSelection(inputArr) {
+  const len = inputArr.length;
+  for (let i = 0; i < len; i++) {
+    let assumedMin = i;
+    for (let j = i + 1; j < len; j++) {
+      if (inputArr[assumedMin] > inputArr[j]) {
+        assumedMin = j;
+      }
+    }
+    if (assumedMin !== i) {
+      let temp = inputArr[i];
+      inputArr[i] = inputArr[assumedMin];
+      inputArr[assumedMin] = temp;
+    }
+  }
+  return inputArr;
+}
+
+console.log(anotherSelection([9, 2, 4, 6, 0]));
